759249a333e2df9255470cd19512e51ae22b661f
dewaupadana <dewegedealitupadana@gmail.com>
2026-03-22 21:32:27 +0700

    add foto

--- src/components/wedding/HeroSection.tsx
+++ src/components/wedding/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import { useScrollAnimation } from '@/hooks/useScrollAnimation';
-import heroBg from '@/assets/couple.jpg';
+import heroBg from '@/assets/TENGANAN-43.jpg';
 import OrnamentDivider from './OrnamentDivider';
 
 const HeroSection = () => {
